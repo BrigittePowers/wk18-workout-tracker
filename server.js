d3726aff9bae2ba86df884a52c1c2a2953c400cf
Brigitte Powers PC <reyhn701@gmail.com>
2021-12-01 15:14:13 -0600

    Added heroku and atlas connections. Updated server.js to navigate first to atlas then default to local setup if unavailable.

--- server.js
+++ server.js
@@ -14,9 +14,15 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/userdb', {
-	useNewUrlParser: true,
-});
+mongoose.connect(
+	process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	},
+);
 
 // submit route example
 app.post('/submit', ({ body }, res) => {
