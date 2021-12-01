153d58ff083ed87c2957f6c4f2c0e60426fd1852
Brigitte Powers PC <reyhn701@gmail.com>
2021-12-01 16:19:01 -0600

    Added route to update a workout

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -27,4 +27,37 @@ module.exports = function (app) {
 				res.json(err);
 			});
 	});
+
+	// add an exercise to a workout
+	app.put('/api/workouts/:id', ({ body, params }, res) => {
+		const workoutId = params.id;
+		let savedExercises = [];
+
+		// grab all exercises in current workout
+		db.Workout.find({ _id: workoutId })
+			.then((dbWorkout) => {
+				// console.log(dbWorkout)
+				savedExercises = dbWorkout[0].exercises;
+				res.json(dbWorkout[0].exercises);
+				let allExercises = [...savedExercises, body];
+				console.log(allExercises);
+				updateWorkout(allExercises);
+			})
+			.catch((err) => {
+				res.json(err);
+			});
+
+		// update current workout
+		function updateWorkout(exercises) {
+			db.Workout.findByIdAndUpdate(
+				workoutId,
+				{ exercises: exercises },
+				function (err, doc) {
+					if (err) {
+						console.log(err);
+					}
+				},
+			);
+		}
+	});
 };
